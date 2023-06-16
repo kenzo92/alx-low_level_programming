ce4cad9258f8ed3e5d67a749b9ac29ae830b77e7
Kenneth <fabiankenneth92@gmail.com>
2023-06-16 05:32:57 -0700

    Created a C Program that peints only single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This Program prints all the single digit numbers
+ * variable i of type int was declared.
+ * Return: main returns valu of data type int
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
