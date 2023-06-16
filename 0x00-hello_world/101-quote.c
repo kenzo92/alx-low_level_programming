41386a1d978fa0cb8cde9a905b8c6f2cbea23663
Kenneth <fabiankenneth92@gmail.com>
2023-06-15 17:56:39 -0700

    Created a program that writes given text to standard error using the write function from unistd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A function that writes a given
+ * text input to standard error(file descriptor 2)
+ * Return: Always return 1.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59\n);
+return (1);
+}
