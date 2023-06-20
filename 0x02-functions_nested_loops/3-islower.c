8f14de8002e7eb1139dd98a46a7c868690ca92b8
Kenneth <fabiankenneth92@gmail.com>
2023-06-20 09:36:25 -0700

    Wrote a Program that returns 1 or 0 depending on if the character is lowercase or otherwise

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * islower - This function returns true for lowercase alphabet
+ * and false for any other character
+ * @c: Parameter used to copy value to be checked
+ *
+ * Return: returns value of type int.
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+	return (1);
+else
+	return (0);
+}
