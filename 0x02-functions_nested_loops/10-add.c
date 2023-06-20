62572e8d70e093b59fe1bc006cc8b7cfe43c666b
Kenneth <fabiankenneth92@gmail.com>
2023-06-20 16:19:34 -0700

    Wrote a program that adds two integer values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - This function adds two integer values
+ * @num1: First parameter
+ * @num2: Second paramter
+ *
+ * Return: returns type int
+ */
+int add(int num1, int num2)
+{
+int result;
+
+result = num1 + num2;
+return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - This function displays the times table
+ *
+ * Description: variables row, column, result, x, and y, all of
+ * type int were declared
+ * Return: Returns void
+ */
+void times_table(void)
+{
+	int row, column, x, y, result;
+for (row = 0; row <= 9; row++)
+{
+for (column = 0; column <= 9; column++)
+{
+result = row * column;
+x = result / 10;
+y = result % 10;
+if (result < 10)
+{
+_putchar(result + '0');
+}
+else
+{
+_putchar(x + '0');
+_putchar(y + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+}
