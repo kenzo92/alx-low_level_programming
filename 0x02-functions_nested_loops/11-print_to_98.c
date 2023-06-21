1b359e8874e22891bf9b155b916dae41e81e01ea
Kenneth <fabiankenneth92@gmail.com>
2023-06-20 17:12:17 -0700

    Wrote a Program that prints natural numbers fron n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - This function prints natural numbers
+ * @n: Parameter that copies the value of intger to
+ * begin printing from
+ *
+ * Return: returns void
+ */
+void print_to_98(int n)
+{
+int i;
+
+i = n;
+if (n == 98)
+{
+printf("%d\n", n);
+}
+else if (n > 98)
+{
+for (n = i; n >= 98; n--)
+{
+printf("%d, ", n);
+}
+printf("\n");
+}
+else if (n == 0 || n < 98)
+{
+for (n = i; n <= 98; n++)
+{
+printf("%d, ", n);
+}
+printf("\n");
+}
+}
