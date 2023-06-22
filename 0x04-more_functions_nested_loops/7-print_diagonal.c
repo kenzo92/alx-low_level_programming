7da2e0c88086e272e213bd1a3ed2ea073b3efee9
Kenneth <fabiankenneth92@gmail.com>
2023-06-22 09:12:47 -0700

    Wrote a Program that prints a diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - This function prints diagonals n-times
+ * @n: number of diagonals to be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (!(n <= 0))
+	{
+	for (i = 1; i <= n; i++)
+	{
+	for (j = 0; j < i; j++)
+	{
+	_putchar(' ');
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
