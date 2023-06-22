8861da356feaf33da33f113dcd4f9ff1478ccb98
Kenneth <fabiankenneth92@gmail.com>
2023-06-22 05:04:21 -0700

    Wrote a Program that multiplies two integer values and returns a result

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - This function multiplies two integers
+ * @a: Parameter that copies first integer value to be multiplied
+ * @b: Parameter that copies second integer value to be multiplied
+ *
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
