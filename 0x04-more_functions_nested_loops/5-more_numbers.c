1719772fdd501aa39493881d1f534237ef951e62
Kenneth <fabiankenneth92@gmail.com>
2023-06-22 07:04:29 -0700

    Modified the Program that prints ten lines of digits from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,27 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
+	char c;
 
 	for (j = 1; j <= 10; j++)
 	{
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 14 ; i++)
 	{
-	_putchar(i);
+	if (i < 10)
+	{
+	c = i + '0';
+	}
+	else
+	{
+	k = i / 10;
+	c = k + '0';
+	_putchar(c);
+	k = i % 10;
+	c = k + '0';
+	}
+	_putchar(c);
 	}
 	_putchar('\n');
 	}
-	_putchar('\n');
 }
