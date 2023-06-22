4ffaec2c1fac4a9803f5a4682a2b7dd9acc7bc4b
Kenneth <fabiankenneth92@gmail.com>
2023-06-22 04:43:00 -0700

    Wrote a Program that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - This function checks for single digits
+ * @c: Parameter that copies the digit value to be checked
+ *
+ * Return: either 1 or 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 58)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
