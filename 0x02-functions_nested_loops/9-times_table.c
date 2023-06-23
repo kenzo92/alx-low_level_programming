b485545476322543abc711202d7cda1f35729c53
Kenneth <fabiankenneth92@gmail.com>
2023-06-23 16:50:43 -0700

    Refactored the times_table function in the 9-times_table.c source file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,30 @@ for (column = 0; column <= 9; column++)
 result = row * column;
 x = result / 10;
 y = result % 10;
-if (result < 10)
+if (column == 9 && result < 10)
 {
 _putchar(result + '0');
 }
-else
+else if (column != 9 && result < 10)
+{
+_putchar(result + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else if (column != 9 && result >= 10)
 {
 _putchar(x + '0');
 _putchar(y + '0');
 _putchar(',');
 _putchar(' ');
 }
-_putchar('\n');
+else if (column == 9 && result >= 10)
+{
+_putchar(x + '0');
+_putchar(y + '0');
 }
+}
+_putchar('\n');
 }
 }
