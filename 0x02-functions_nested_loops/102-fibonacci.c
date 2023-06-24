0f83b3f41709ba6b7c40e781e3499bd291817de6
Kenneth <fabiankenneth92@gmail.com>
2023-06-23 18:23:08 -0700

    Wrote a program that prints the first fifty terms of the Fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This function prints the first fifty terms of
+ * the Fibonacci sequence. Variables first_num, second_num, and
+ * fibonacci, each of the long long int data type are declared
+ * Result: Void
+ */
+
+int main(void)
+{
+	long int first_num, second_num, fibonacci;
+	int i;
+
+	first_num = 1;
+	second_num = 2;
+	printf("%ld, ", first_num);
+	printf("%ld, ", second_num);
+
+	for (i = 3; i <= 50; i++)
+	{
+	fibonacci = first_num + second_num;
+	if (i != 50)
+	{
+	printf("%ld, ", fibonacci);
+	first_num = second_num;
+	second_num = fibonacci;
+	}
+	else
+	{
+	printf("%ld", fibonacci);
+	}
+	}
+	printf("\n");
+	return (0);
+}
