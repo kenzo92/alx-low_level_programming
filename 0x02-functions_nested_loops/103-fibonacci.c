9837322e5a63eba6012ae3d8402d64c9271c63a6
Kenneth <fabiankenneth92@gmail.com>
2023-06-23 19:02:29 -0700

    Wrote a program that returns the sum of even Fibonacci rems below 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: This function prints the sum of all the even Fibonacci
+ * terms below 4000000
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int sum, first_num, second_num, fibonacci;
+	int i;
+
+	first_num = 1;
+	second_num = 2;
+	sum = 3;
+
+	for (i = 3; i < 50; i++)
+	{
+	fibonacci = first_num + second_num;
+	if (fibonacci <= 4000000 && fibonacci % 2 == 0)
+	{
+	sum += fibonacci;
+	}
+	first_num = second_num;
+	second_num = fibonacci;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
