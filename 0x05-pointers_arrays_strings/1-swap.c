9112c278698a6ceea1f2a27f4f7e5663474ce20d
Kenneth <fabiankenneth92@gmail.com>
2023-06-26 06:42:31 -0700

    Modified the function that swaps values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,16 @@
  * swap_int - This function swaps integer values
  * @a: Parameter that points to the first value to be swapped
  * @b: Parameter that points to the second value to be swapped
+ * @temp: Parameter used to temporarily hold the value of a
  *
  * Return: Nothing
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
