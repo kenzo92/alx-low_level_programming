56fa8e189bfd301f25690595edcdd3e7890683fd
Kenneth <fabiankenneth92@gmail.com>
2023-06-26 06:45:30 -0700

    Modified the function that swaps values

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,7 +4,6 @@
  * swap_int - This function swaps integer values
  * @a: Parameter that points to the first value to be swapped
  * @b: Parameter that points to the second value to be swapped
- * @temp: Parameter used to temporarily hold the value of a
  *
  * Return: Nothing
  */
