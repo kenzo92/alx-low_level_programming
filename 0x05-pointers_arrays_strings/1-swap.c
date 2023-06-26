39ced4545a6e2fcf39f06b5a9e78fea26907036e
Kenneth <fabiankenneth92@gmail.com>
2023-06-26 06:35:32 -0700

    Wrote a function that swaps values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - This function swaps integer values
+ * @a: Parameter that points to the first value to be swapped
+ * @b: Parameter that points to the second value to be swapped
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
