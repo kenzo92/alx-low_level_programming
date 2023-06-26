de2ce5f72b94be1707912bf7156e89279cb72e95
Kenneth <fabiankenneth92@gmail.com>
2023-06-26 11:18:26 -0700

    Wrote a Program that prints half the characters in a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * puts_half - This function prints half of a string
+ * @str: Parameter that copies the string to be printed
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, n, length_of_the_string;
+
+	length_of_the_string = 0;
+	while (str[length_of_the_string])
+	{
+	length_of_the_string++;
+	}
+
+	if (length_of_the_string % 2 == 0)
+	{
+	n = length_of_the_string / 2;
+	for (i = n; i <= length_of_the_string; i++)
+	{
+	_putchar(str[i]);
+	}
+	}
+
+	else if (length_of_the_string % 2 == 1)
+	{
+	n = (length_of_the_string - 1) / 2;
+	for (i = n; i <= length_of_the_string; i++)
+	{
+	_putchar(str[i]);
+	}
+	}
+	_putchar('\n');
+}
