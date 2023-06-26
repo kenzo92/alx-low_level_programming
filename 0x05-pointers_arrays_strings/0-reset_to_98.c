0464a3ac81829bcb01848cf7ef2d8cd8c2a48b67
Kenneth <fabiankenneth92@gmail.com>
2023-06-26 06:15:14 -0700

    Wrote a program that updates value of a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - Updates the value it points to
+ * @n: Parameter that points to the address of the variable it updates
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory demonstrates introductory knowledge to Pointers, Arrays, and Strings in C Programming
