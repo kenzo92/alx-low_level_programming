04dc89fdaefe657f0ad14c73430a6c69ea083772
Kenneth <fabiankenneth92@gmail.com>
2023-07-13 08:18:33 -0700

    Wrote a program that returns an array using the malloc function

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 
 #endif
