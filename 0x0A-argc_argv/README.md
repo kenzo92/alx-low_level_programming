1a2560ef6d5faa199b61ddbed79e34bfb8e8ef09
kenzo92 <104776906+kenzo92@users.noreply.github.com>
2023-07-20 12:14:38 +0100

    Delete 0x0A-argc_argv directory

--- 0x0A-argc_argv/README.md
+++
@@ -1,2 +0,0 @@
-This directory contains the algorithms that illustrate the use of argc and argv
-parameters in the main function
--- 0x0A-argc_argv/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int main(int argc, char *argv[]);
-int _putchar(char c);
-int is_positive_number(char *str);
-
-#endif /* MAIN_H */
