02a5270973056210a1c0d2cfca7c8aee16d755c1
kenzo92 <104776906+kenzo92@users.noreply.github.com>
2023-07-20 12:17:51 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef _ABSOLUTE_VALUE
-#define _ABSOLUTE_VALUE
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-
-#endif
--- 0x0D-preprocessor/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains algorithms on C macros(Object-type and function-type macros)
